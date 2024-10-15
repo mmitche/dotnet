c0ba910a174452d2851422858b15aa1c17f6fa06
Matt Mitchell (.NET) <mmitche@microsoft.com>
2024-10-15 14:02:21 -0700

    Add force dry run signing

--- eng/build.ps1
+++ eng/build.ps1
@@ -67,7 +67,11 @@ if ($test) {
 }
 
 if ($sign) {
-  $argument += "/p:Sign=true"
+  $arguments += "/p:Sign=true"
+  # Force dry run signing for now. In typical VMR builds, the official build ID is set for each repo, which
+  # tells the signing infra that it should expect to see signed bits. This won't be the case in CI builds,
+  # and won't be the case for official builds until more of the real signing infra is functional.
+  properties+=( "/p:ForceDryRunSigning=true" )
 }
 
 if ($buildRepoTests) {
