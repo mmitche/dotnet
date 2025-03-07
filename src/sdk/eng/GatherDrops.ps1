2463e71fcefa630d2f13053430ba3e58b76ef251
Matt Mitchell (.NET) <mmitche@microsoft.com>
2025-03-07 13:47:16 -0800

    WIP

--- src/sdk/eng/GatherDrops.ps1
+++ src/sdk/eng/GatherDrops.ps1
@@ -6,9 +6,7 @@ param(
   [String]$outputPath,
   [Parameter(Mandatory=$true)]
   [String]$darcPath,
-  [Parameter(Mandatory=$true)]
   [String]$githubPat,
-  [Parameter(Mandatory=$true)]
   [String]$azdevPat
 )
 $jsonContent = Get-Content -Path $filePath -Raw | ConvertFrom-Json
@@ -16,7 +14,7 @@ foreach ($repo in $jsonContent.repositories) {
     $remoteUri = $repo.remoteUri
     $commitSha = $repo.commitSha
     $path = "$outputPath$($repo.path)"
-    $darcCommand = "$darcPath gather-drop -c $commitSha -r $remoteUri --non-shipping --skip-existing --continue-on-error --use-azure-credential-for-blobs -o $path --github-pat $githubPat --azdev-pat $azdevPat --verbose --ci"
+    $darcCommand = "$darcPath gather-drop -c $commitSha -r $remoteUri --non-shipping --skip-existing --continue-on-error --use-azure-credential-for-blobs -o $path --verbose --ci"
     Write-Output "Gathering drop for $remoteUri"
     Invoke-Expression $darcCommand
 }
